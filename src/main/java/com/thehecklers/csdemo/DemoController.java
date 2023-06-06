b665f8ac88a920ade90c349ad4e5cfaaf7255c36
Mark Heckler <mark.heckler@gmail.com>
2023-06-06 15:24:21 -0500

    Yolo

---
+++ src/main/java/com/thehecklers/csdemo/DemoController.java
@@ -0,0 +1,19 @@
+package com.thehecklers.csdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+    @GetMapping
+    public String alive() {
+        return "Codespaces demo is alive!";
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
---
+++ src/test/java/com/thehecklers/csdemo/CsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.csdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
